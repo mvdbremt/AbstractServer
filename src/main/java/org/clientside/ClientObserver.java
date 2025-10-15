2cfbc7cec6a24512442d854f63e1acb7f78d8f35
mvdbremt <mathieuvandenbremt@gmail.com>
2025-10-15 16:12:08 +0200

    added comments

--- src/main/java/org/clientside/ClientObserver.java
+++ src/main/java/org/clientside/ClientObserver.java
@@ -4,7 +4,12 @@ package org.clientside;
 import java.net.Socket;
 
 public interface ClientObserver {
+    // makes user handle connection to server
     public void handleConnection(Socket client);
+
+    // makes user handle shutdown of client
     public void handleClientShutdown(Client client);
+
+    // called when client receives message
     public void receiveMessage(String message);
 }
--- src/main/java/org/serverside/ServerObserver.java
+++ src/main/java/org/serverside/ServerObserver.java
@@ -3,9 +3,15 @@ package org.serverside;
 import java.net.Socket;
 
 public interface ServerObserver {
-
+    // makes user handle connection of new socket
     public void handleConnection(Socket client);
+
+    // makes user handle shutdown of server
     public void handleServerShutdown(Server server);
+
+    // makes user handle shutdown of a client
     public void handleClientShutdown(Server.ConnectionHandler client);
+
+    // called when server receives message
     public void receiveMessage(String message, Server.ConnectionHandler connectionHandler);
 }
